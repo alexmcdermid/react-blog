c4cf0932f8be24289d6b8a20f8a8c9c364fffe7b
alexmcdermid <alexander.mcdermid@gmail.com>
2021-11-08 08:48:35 -0800

    add form and states to react portfolio

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import Portfolio from './pages/Portfolio/Portfolio';
 import About from './pages/About/About'
 
 class App extends Component {
+  state = {
+    name:"",
+    email:"",
+    message:"",
+  }
+  handleFormChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         <Link to='/about'>About    </Link>
         <Link to='/skills'>Skills    </Link>
-        <Link to='/portfolio'>Portfolio</Link>
+        <Link to='/portfolio'>Portfolio   </Link>
+
+       
+
         <Switch>
         <Route path='/about' render={(props) => (
             <About {...props}/>
@@ -22,8 +39,43 @@ class App extends Component {
         <Route path='/portfolio' render={(props) => (
             <Portfolio {...props}/>
           )}/>
+         
           <Redirect to="/about" />
         </Switch>
+        <h3>Some form on everypage for contact</h3>
+        <form>
+          <label>
+            <span>
+                Name
+            </span>
+            <input
+            name="name"
+            value={this.state.name}
+            onChange={this.handleFormChange}/>    
+          </label>
+          <br/>
+          <label>
+            <span>
+                Email
+            </span>
+            <input
+            name="email"
+            value={this.state.email}
+            onChange={this.handleFormChange}/>    
+          </label>
+          <br/>
+          <label>
+            <span>
+                Message
+            </span>
+            <input
+            name="message"
+            value={this.state.message}
+            onChange={this.handleFormChange}/>    
+          </label>
+          <br/>
+          <button onClick={this.handleFormSubmit}>Send</button>
+          </form>
       </div>
     );
   }
